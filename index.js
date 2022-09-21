7312aae098fb7abb3a3ea848c017197ed1e20404
Mouhamed <mouhamed-el-moustapha.mboup@epitech.eu>
2022-09-21 22:48:04 +0200

    Done

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const app =express();
+const app = express();
+require('./models/dbConfig');
+const postsRoutes = require('./routes/postsController');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-require('./models/dbConfig')
 
-app.listen(5500, () => {
-    console.log('Server is running on port 5500');
-})
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/posts', postsRoutes);
+
+app.listen(5500, () => console.log('Server started: 5500'));
