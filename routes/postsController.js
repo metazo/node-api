ac50efc983260c03968d9c9da06faf73c3f10d9e
Mouhamed <mouhamed-el-moustapha.mboup@epitech.eu>
2022-09-21 21:37:37 +0200

    initial commit

--- routes/postsController.js
+++ routes/postsController.js
@@ -2,3 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { PostsModel } = require('../models/postsModel')
+
+router.get('/', (req, res) => {
+    PostsModel.find((err, docs) => {
+        console.log(docs);
+    })
+})
+
+module.exports = router
